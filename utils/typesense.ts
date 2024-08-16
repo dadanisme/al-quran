294a6c9d9bff7f29c9ffed4ffd670415e642283e
dadanisme <dadanisme@upi.edu>
2024-08-17 03:27:12 +0700

    change api key

--- utils/typesense.ts
+++ utils/typesense.ts
@@ -3,12 +3,12 @@ import { SearchClient } from "typesense";
 const client = new SearchClient({
   nodes: [
     {
-      host: "l4kbshr1jayo6tg7p-1.a1.typesense.net",
+      host: "dguhimexws2lfy3qp-1.a1.typesense.net",
       port: 443,
       protocol: "https",
     },
   ],
-  apiKey: "ezVUWYla2m137GP0Pvw7rsp85zz0lxDV",
+  apiKey: "6CkArWgvfO9voY7QrgWlLxrJbkghmgo4",
 });
 
 export default client;
